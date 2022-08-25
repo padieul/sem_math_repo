cfa80af146802b9cacb1b6300b4b9992dcced98f
Paul Dietze <prdietze@outlook.de>
2022-08-25 10:12:48 +0200

    latex2mathmldemo added

--- latex2mathmldemo.py
+++ latex2mathmldemo.py
@@ -4,6 +4,6 @@ import latex2mathml.converter
 
 if __name__ == "__main__":
 
-    latex_input = "(a+b=b+a)"
+    latex_input = "\sqrt{2}"
     mathml_output = latex2mathml.converter.convert(latex_input)
     print(mathml_output)
