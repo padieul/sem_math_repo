b5238955c9675d6fe736198dcf0db8aadc0a0238
Paul Dietze <prdietze@outlook.de>
2022-09-18 14:20:45 +0200

    posts thread extraction func added

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import xml.dom.minidom
 import xmltodict, json
-
-
+from tqdm import tqdm
+import glob
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
 
 
@@ -56,6 +58,67 @@ def slice_json_file(rel_dir_part, file_name ,rows_size):
     print(json.dumps(data["rows"][0], indent=4, sort_keys=False))
 
 
+## ----------------------------------------------------------------------------------------
+
+def get_db_settings(s_filename):
+    
+    with open(s_filename, "r") as f:
+        db_settings = json.load(f)
+    f.close()
+    return db_settings
+
+
+def get_mongo_db(settings): 
+
+    MONGODB_USER_NAME = settings["username"] 
+    MONGODB_PASSWORD = settings["password"] 
+    MONGODB_HOST = settings["host"] 
+    MONGODB_PORT = settings["port"] 
+    MONGODB_SOURCE = settings["db"]
+    MONGODB_AUTHENTICATION_DB = settings["db"]
+
+    uri_str = "mongodb://" + MONGODB_USER_NAME + ":" + MONGODB_PASSWORD + "@" + MONGODB_HOST + ":" + str(MONGODB_PORT)
+    client = MongoClient(uri_str)
+    
+    try:
+        client.admin.command("ping")
+    except ConnectionFailure: 
+        print("Server not available")
+
+    db = client[MONGODB_AUTHENTICATION_DB]
+    return db
+
+def post_threads_to_db(db, coll_name, post_file_name):
+    QUESTION_POST_ID = 1
+    ANSWER_POST_ID = 2
+    
+
+    with open(post_file_name, "r") as f:
+        posts = json.load(f)
+    f.close()
+
+    thread_counter = 0
+    for row in tqdm(posts["rows"]):
+        if row["@PostTypeId"] == str(QUESTION_POST_ID):
+            thread_dict = {}
+            thread_dict["thread_id"] = "thread" + "_" + row["@Id"] + "_" + str(thread_counter) 
+            thread_dict["ini_post_id"] = row["@Id"]
+            thread_dict["posts"] = [row,]
+            thread_id = db[coll_name].insert_one(thread_dict).inserted_id
+            thread_counter += 1
+
+    for row in tqdm(posts["rows"]):
+        if row["@PostTypeId"] == str(ANSWER_POST_ID):
+            try:
+                parent_id = row["@ParentId"]
+                question_thread_posts = db[coll_name].find_one({"ini_post_id": str(parent_id)})["posts"]
+                question_thread_posts.append(row)
+                db[coll_name].update_one({"ini_post_id": str(parent_id)}, {"$set": {"posts": question_thread_posts}})
+            except:
+                continue
+
+    return thread_counter
+    
 
 if __name__ == "__main__":
     
@@ -66,5 +129,21 @@ if __name__ == "__main__":
                                     limiting_tags=["<votes>", "</votes>"]) #["<posts>", "</posts>"] # "<tags>", "</tags>"
     print("count: ", posts_count)
     """
-
+    """
     slice_json_file("math.stackexchange.com\\posts_json_slices", "json_output100000.json", 100)
+    """ 
+
+    sett = get_db_settings("db_conf.json")
+    datab = get_mongo_db(sett) 
+
+    saved_file_name = "math.stackexchange.com\posts_json_slices\json_output100000.json"
+
+    post_file_names = glob.glob("math.stackexchange.com\posts_json_slices\*")
+    post_file_names.sort()
+    for post_file_name in post_file_names:
+        if post_file_name == saved_file_name:
+            continue
+        print("Filename: ", post_file_name)
+        count = post_threads_to_db(datab, "threads", post_file_name)
+        print("Total number of threads: ", count)
+    
