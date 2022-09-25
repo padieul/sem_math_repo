443f98286d15b7be6d3f2e40ea0e9369652fc6ad
Paul Dietze <prdietze@outlook.de>
2022-09-25 20:32:01 +0200

    Delete latex2mathmldemo.py

--- latex2mathmldemo.py
+++
@@ -1,9 +0,0 @@
-
-import latex2mathml.converter
-
-
-if __name__ == "__main__":
-
-    latex_input = "\sqrt{2}"
-    mathml_output = latex2mathml.converter.convert(latex_input)
-    print(mathml_output)
